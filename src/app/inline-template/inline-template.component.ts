6b42e9d99d20a225fcea0d53ef46325e6f30bc3e
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-09 18:01:48 +0200

    inline multiline styles added

--- src/app/inline-template/inline-template.component.ts
+++ src/app/inline-template/inline-template.component.ts
@@ -4,7 +4,16 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-inline-template',
   // WE DISCARD TEMPLATEURL - templateUrl: './inline-template.component.html', SINCE WE USE INLINE TEMPLATE
   template: '<h4>I\'m just a {{word}} string(((</h4>', // AND IT SOMEHOW ONLY RECOGNIZES SINGLE QUOTES
-  styleUrls: ['./inline-template.component.css']
+  styles: [`
+    h4 {
+      color: darkred;
+      border-radius: .5em;
+      background-color: lightpink;
+      height: 2.5em;
+      padding-top: 1em;
+      text-align: center;
+    }
+  `] // TILDE QUOTES TO MAKE MULTILINE STYLE OR TEMPLATE STRINGS
 })
 export class InlineTemplateComponent implements OnInit {
 
