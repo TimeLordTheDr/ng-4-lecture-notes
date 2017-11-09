f0ad08799b36d9a68732e5fbd4080e40b83b0319
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-09 11:50:25 +0200

    New component created and linked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({ // COMPONENT DECORATOR
+  selector: 'app-root', // DEFINE THE TAG WE USE IN INDEX.HTML
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { // MODULE.EXPORTS = app-root (CamelCased)
   title = 'app';
 }
+
+// 1. Components r generated with "ng g component my-new-component"
+// *.spec.ts file is used for unit testing
+// 2. imports {MyNewComponentComponent} to app.modules.ts
