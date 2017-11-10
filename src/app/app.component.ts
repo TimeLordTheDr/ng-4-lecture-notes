4272ddc88e938b0278fcf4a0c2256184dee6d57d
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-10 18:10:02 +0200

    added comments regarding deploy workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,3 +23,9 @@ export class AppComponent { // MODULE.EXPORTS = app-root (CamelCased)
 // 7. Import that component into main MODULE.TS file with "BrowserAnimationsModule". Note, you must check which name is to import
 // 8. List additional components in "imports" array
 
+// *******************-----BUILD-----***************************
+// 9. Run "ng build" or "npm run build"(in contrast to "start" script it must be used with "run" option)
+// --> Creates dist folder with concated files
+// 10. "ng build --prod"(can not be replaced with "npm" alias). Rebuilds dist filder with minified files
+// 11. "ng build --prod --base-href /home"('/'-root is opt by default). U need to specify the endpoint in
+// case u want to put ur website into place other than default root route
