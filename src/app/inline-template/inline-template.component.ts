a1e129672c79fc02fdacdd169030f7d84055e000
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-10 16:53:55 +0200

    ng anim lib was removed. Continue on

--- src/app/inline-template/inline-template.component.ts
+++ src/app/inline-template/inline-template.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inline-template',
   // WE DISCARD TEMPLATEURL - templateUrl: './inline-template.component.html', SINCE WE USE INLINE TEMPLATE
-  template: '<h4>I\'m just a {{word}} string(((</h4>', // AND IT SOMEHOW ONLY RECOGNIZES SINGLE QUOTES
+  template: '<h4>I\'m just a "{{word}}" string</h4>', // AND IT SOMEHOW ONLY RECOGNIZES SINGLE QUOTES
   styles: [`
     h4 {
       color: darkred;
@@ -17,7 +17,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InlineTemplateComponent implements OnInit {
 
-  word = 'template.js(!!!)';
+  word = 'template.js(!)';
 
   constructor() { }
 
