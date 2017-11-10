eb9a8ded94d81bad85f1fa1f467735381b345f84
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-10 16:37:46 +0200

    Comment on how to add extra packages added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent { // MODULE.EXPORTS = app-root (CamelCased)
   title = 'app';
 }
 
+// *****************-----MAIN FLOW-----*************************
 // 0. Create a proj with ng new projectName [--style=scss || --style=sass || --style=less] to make it create already sass-styled project
 // 1. Components r generated with "ng g component my-new-component"
 // *.spec.ts file is used for unit testing
@@ -16,3 +17,8 @@ export class AppComponent { // MODULE.EXPORTS = app-root (CamelCased)
 // 3. use [ngDir]="proFromTSFile"-notation for attr binding and {{propFromTSFile}} for innerHTML binding
 // 4. Add service with the same syntax as component into MODULE file. Still include it into "PROVIDER" array
 // 5. Import service to the component you want tot use it in and exploit it as "constructor(private serviceAlias:ImportedServiceName)"
+
+// *****************************************************EXRAS-->
+// 6. "npm install @angular/animations --save" additional component
+// 7. Import that component into main MODULE.TS file with "BrowserAnimationsModule". Note, you must check which name is to import
+
